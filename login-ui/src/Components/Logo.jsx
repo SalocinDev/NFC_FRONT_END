d8791a7a8ac3fa0478f69662bdb1563d8c85c925
Medimicolo <johnmichaelunamayor28@gmail.com>
2025-07-24 16:46:56 +0800

    Update Logo.jsx

--- login-ui/src/Components/Logo.jsx
+++ login-ui/src/Components/Logo.jsx
@@ -1,6 +1,5 @@
-function Logo({ src = '/src/Logo/default.png', alt = 'Logo' }) {
+function Logo({ src = '/src/Logo/B-lugo.png', alt = 'Logo' }) {
   return <img src={src} alt={alt} />;
 }
-
 
 export default Logo;
