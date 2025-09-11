bb5845e2d2b509e1c1bf574cded625e99d454f3e
Nicholas <nicholaslonganilla@gmail.com>
2025-09-11 20:19:26 +0800

    survey

--- login-ui/src/Main-Components/Intemediary.jsx
+++ login-ui/src/Main-Components/Intemediary.jsx
@@ -1,15 +1,52 @@
 import { useNavigate } from 'react-router-dom';
-import { Button } from '../Components/index';
+import { toNFClogin } from '../Services/LoginService';
+import classes from '../CSS-Folder/Intemediary.module.css';
+import SurveyWhite from '../Logo/SurveyWhite.png';
+import { Button, LogoComponent} from '../Components';
+import { useState, useEffect, useRef } from 'react';
 
-function Intermediary() {
-    const navigate = useNavigate();
+function NfcPage() {
+  
+     const navigate = useNavigate();
 
-    return (
-        <div>
-            <Button name="Account" use="ButtonUserPage" onClick={() => navigate("/UserPage")}/>
+  return (
+    <div className={classes.body}>
+      <div className={classes.curvedRectangle}>
+        <div className={classes.content}>
+          <div className={classes.tagline}>
+            <img src={SurveyWhite} alt="Logo" className={classes.NfcLogo} />
+            
             <Button name="Services" use="ButtonServices" onClick={() => navigate("/Services")}/>
+          </div>
+          
         </div>
-    );
+        
+      </div>
+            <div className={classes.Ewan}>
+              <div className={classes.Logo}>
+                
+                <LogoComponent/>
+                <p className={classes.Text}>
+                  New to our platform? Sign Up now.
+                </p>
+
+              <div>        
+                <Button name="Account" use="ButtonUserPage" onClick={() => navigate("/UserPage")}/>
+              </div>
+
+              </div>
+            </div>
+    </div>
+
+    
+  );
 }
 
-export default Intermediary;
+export default NfcPage;
+
+
+
+           
+            
+
+            
