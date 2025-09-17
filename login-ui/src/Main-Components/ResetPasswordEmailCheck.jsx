f2a379f6f5d446d2b22a94183322b139e4140513
Nicholas <nicholaslonganilla@gmail.com>
2025-09-17 19:37:01 +0800

    ResetPassword Mobile View

--- login-ui/src/Main-Components/ResetPasswordEmailCheck.jsx
+++ login-ui/src/Main-Components/ResetPasswordEmailCheck.jsx
@@ -43,7 +43,7 @@ function ResetPasswordEmailCheck() {
       <h1 className={classes.title}>Reset Password</h1>
       <p className={classes.subtitle}>Please enter your email</p>
 
-      <Input placeholder='Enter Email' type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+      <Input  className={classes.InputEmailReset} placeholder='Enter Email' type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
       <Button name="Verify Email" use="ButtonReset" onClick={handleCheckEmail} />
       
     </div>
