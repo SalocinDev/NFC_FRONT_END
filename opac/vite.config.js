ed367dcf92154ae2bc625ac2281d5fd83fd9a005
Nicholas <nicholaslonganilla@gmail.com>
2025-11-07 21:26:18 +0800

    try fix production viteconfig

--- opac/vite.config.js
+++ opac/vite.config.js
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NFC_FRONT_END/opac/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
+  base: "/NFC_FRONT_END/opac/",
   server: {
     host: '0.0.0.0',
     port: 5001,
     allowedHosts: ['manila.city.library'],
   },
-})
+  resolve: {
+    dedupe: [
+      'react',
+      'react-dom',
+      '@emotion/react',
+      '@emotion/styled'
+    ],
+    alias: {
+      // Force Vite to use local node_modules, not ../login-ui
+      '@mui': '/srv/Capstone/NFC_FRONT_END/opac/node_modules/@mui',
+      '@emotion': '/srv/Capstone/NFC_FRONT_END/opac/node_modules/@emotion'
+    }
+  }
+});
